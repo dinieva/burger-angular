dc5fa5605e4df1ec473d36ca8e1f47733f5acad5
Dinieva Rezeda <dinievarezeda@mail.ru>
2024-05-07 17:14:06 +0300

    change paths to images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
 
   productsData = [
     {
-      image: "./assets/images/1.png",
+      image: "assets/images/1.png",
       title: 'Бургер чеддер & бекон',
       text: 'Котлета из говядины криспи, булочка, томат, сыр Чеддер, грудинка, лук красный, салат айсбер, майонез, кетчуп, сырный соус',
       price: 8,
@@ -26,7 +26,7 @@ export class AppComponent {
       grams: 360,
     },
     {
-      image: "./assets/images/2.png",
+      image: "assets/images/2.png",
       title: 'BBQ с беконом и курицей',
       text: 'Булочка бриошь с кунжутом, куриная котлета, сыр чеддер, томат, огурец маринованный, лук маринованный, салат Ромен, бекон, соус BBQ',
       price: 7,
@@ -34,7 +34,7 @@ export class AppComponent {
       grams: 390,
     },
     {
-      image: "./assets/images/3.png",
+      image: "assets/images/3.png",
       title: 'Дабл биф бургер',
       text: 'Две говяжьи котлеты, сыр чеддер, салат романо, маринованные огурцы, свежий томат, бекон, красный лук, соус бургер, горчица',
       price: 10,
@@ -42,7 +42,7 @@ export class AppComponent {
       grams: 420,
     },
     {
-      image: "./assets/images/4.png",
+      image: "assets/images/4.png",
       title: 'Баварский бургер',
       text: 'Булочка для бургера, говяжья котлета, красный лук, сыр, охотничья колбаска, соус барбекю, соус сырный, салат айсберг',
       price: 7,
@@ -50,7 +50,7 @@ export class AppComponent {
       grams: 220,
     },
     {
-      image: "./assets/images/5.png",
+      image: "assets/images/5.png",
       title: 'Бекон чизбургер',
       text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, сыр, сырный соус, кетчуп, зелень',
       price: 8,
@@ -58,7 +58,7 @@ export class AppComponent {
       grams: 220,
     },
     {
-      image: "./assets/images/6.png",
+      image: "assets/images/6.png",
       title: 'Индиана бургер',
       text: 'Булочка для бургера, котлета куриная, грудинка, яйцо, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
       price: 9,
@@ -66,7 +66,7 @@ export class AppComponent {
       grams: 320,
     },
     {
-      image: "./assets/images/7.png",
+      image: "assets/images/7.png",
       title: 'Вегги бургер',
       text: 'Булочка для бургера, вегетарианская котлета, красный лук, сыр, свежий томат, соус барбекю, соус сырный, салат айсберг',
       price: 8,
@@ -74,7 +74,7 @@ export class AppComponent {
       grams: 280,
     },
     {
-      image: "./assets/images/8.png",
+      image: "assets/images/8.png",
       title: 'Плаксивый Джо',
       text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, красный лук, сыр, перец халапеньо, кетчуп, зелень',
       price: 7,
@@ -82,7 +82,7 @@ export class AppComponent {
       grams: 380,
     },
     {
-      image: "./assets/images/9.png",
+      image: "assets/images/9.png",
       title: 'Двойной чиз бургер',
       text: 'Булочка для бургера, две говяжьи котлеты, двойной сыр чеддар, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
       price: 11,
@@ -90,7 +90,7 @@ export class AppComponent {
       grams: 400,
     },
     {
-      image: "./assets/images/10.png",
+      image: "assets/images/10.png",
       title: 'Фрешбургер',
       text: 'Булочка для бургера, говяжья котлета, бекон, сыр чеддар, яйцо, салями, соус барбекю, соус сырный, салат айсберг, свежий томат',
       price: 9,
@@ -98,7 +98,7 @@ export class AppComponent {
       grams: 300,
     },
     {
-      image: "./assets/images/11.png",
+      image: "assets/images/11.png",
       title: 'Цуккини бургер',
       text: 'Булочка для бургера, вегетарианская котлета из нута, цуккини на гриле, помидор, огурец маринованный, сыр, горчичный соус, кетчуп, зелень',
       price: 8,
@@ -106,7 +106,7 @@ export class AppComponent {
       grams: 320,
     },
     {
-      image: "./assets/images/12.png",
+      image: "assets/images/12.png",
       title: 'Двойной бургер чеддар',
       text: 'Булочка для бургера, котлета говяжья, грудинка, красный лук, огурец маринованный, томат, кетчуп, двойной сыр чеддар, горчица, зелень',
       price: 9,
